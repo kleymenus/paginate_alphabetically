632b9b881e8fbafa1238c8c6a3e35cd1dc015133
Alexander Kleymenov <alexander@kleymenov.net>
2013-06-27 09:20:18 +0400

    Update active_record.rb

--- lib/paginate_alphabetically/active_record.rb
+++ lib/paginate_alphabetically/active_record.rb
@@ -1,5 +1,5 @@
 module PaginateAlphabetically
-  ALL_LETTERS = ('A'..'Z')+('а'..'я').to_a
+  ALL_LETTERS = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','а','б','в','г','л','е','ж','з','и','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','э','ю','я').to_a
   module ActiveRecord
     def paginate_alphabetically(params)
       @attribute = params[:by]
