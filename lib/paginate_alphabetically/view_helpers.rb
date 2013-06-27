8e9c554e6dec6e4bafa0f6ade66f61a5b12e2c38
Alexander Kleymenov <unit@hasmind.net>
2013-06-27 09:35:22 +0400

    literals

--- lib/paginate_alphabetically/view_helpers.rb
+++ lib/paginate_alphabetically/view_helpers.rb
@@ -19,7 +19,7 @@ module PaginateAlphabetically
     end
 
     def alphabetical_links_to(available_letters)
-      ('A'..'Z').map do |letter|
+      ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','а','б','в','г','л','е','ж','з','и','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','э','ю','я'].map do |letter|
         content_tag(:li, paginated_letter(available_letters, letter))
       end.join(" ")
     end
